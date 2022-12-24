6afa65714929daa3547e1f194a73aa0b8fef9496
zapteryx <zapteryx@users.noreply.github.com>
2022-12-24 10:34:30 +0000

    refactor: prettify

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,13 @@
 import type { Config } from '@jest/types';
 // Sync object
 const config: Config.InitialOptions = {
-	roots: ['<rootDir>/src'],
-	transform: {
-		'^.+\\.tsx?$': ['ts-jest', { useESM: true }],
-	},
-	moduleNameMapper: {
-		'(.+)\\.js': '$1',
-	},
-	extensionsToTreatAsEsm: ['.ts'],
+    roots: ['<rootDir>/src'],
+    transform: {
+        '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
+    },
+    moduleNameMapper: {
+        '(.+)\\.js': '$1',
+    },
+    extensionsToTreatAsEsm: ['.ts'],
 };
 export default config;
--- src/index.ts
+++ src/index.ts
@@ -10,28 +10,28 @@ import { TimeFormats, TimeObject } from './index.d.js';
 export function msToTime(milliseconds: number, format: TimeFormats): number;
 export function msToTime(milliseconds: number): TimeObject;
 export function msToTime(
-	milliseconds: number,
-	format?: TimeFormats
+    milliseconds: number,
+    format?: TimeFormats,
 ): TimeObject | number {
-	const total_seconds = Math.floor(milliseconds / 1000);
-	const total_minutes = Math.floor(total_seconds / 60);
-	const total_hours = Math.floor(total_minutes / 60);
-	const days = Math.floor(total_hours / 24);
-	const seconds = total_seconds % 60;
-	const minutes = total_minutes % 60;
-	const hours = total_hours % 24;
-	switch (format) {
-		case 's':
-			return total_seconds;
-		case 'm':
-			return total_minutes;
-		case 'h':
-			return total_hours;
-		case 'd':
-			return days;
-		default:
-			return { d: days, h: hours, m: minutes, s: seconds };
-	}
+    const total_seconds = Math.floor(milliseconds / 1000);
+    const total_minutes = Math.floor(total_seconds / 60);
+    const total_hours = Math.floor(total_minutes / 60);
+    const days = Math.floor(total_hours / 24);
+    const seconds = total_seconds % 60;
+    const minutes = total_minutes % 60;
+    const hours = total_hours % 24;
+    switch (format) {
+        case 's':
+            return total_seconds;
+        case 'm':
+            return total_minutes;
+        case 'h':
+            return total_hours;
+        case 'd':
+            return days;
+        default:
+            return { d: days, h: hours, m: minutes, s: seconds };
+    }
 }
 
 /**
@@ -41,36 +41,36 @@ export function msToTime(
  * @returns The converted string.
  */
 export function msToTimeString(
-	msObject: TimeObject,
-	simple?: boolean
+    msObject: TimeObject,
+    simple?: boolean,
 ): string | 'MORE_THAN_A_DAY' {
-	if (simple) {
-		if (msObject['d'] > 0) {
-			return 'MORE_THAN_A_DAY';
-		}
-		return `${msObject['h'] > 0 ? `${msObject['h']}:` : ''}${
-			msObject['h'] > 0
-				? msObject['m'].toString().padStart(2, '0')
-				: msObject['m']
-		}:${msObject['s'].toString().padStart(2, '0')}`;
-	}
-	return `${
-		msObject['d'] > 0
-			? `${msObject['d']} day${msObject['d'] === 1 ? '' : 's'}, `
-			: ''
-	}${
-		msObject['h'] > 0
-			? `${msObject['h']} hr${msObject['h'] === 1 ? '' : 's'}, `
-			: ''
-	}${
-		msObject['m'] > 0
-			? `${msObject['m']} min${msObject['m'] === 1 ? '' : 's'}, `
-			: ''
-	}${
-		msObject['s'] > 0
-			? `${msObject['s']} sec${msObject['s'] === 1 ? '' : 's'}, `
-			: ''
-	}`.slice(0, -2);
+    if (simple) {
+        if (msObject['d'] > 0) {
+            return 'MORE_THAN_A_DAY';
+        }
+        return `${msObject['h'] > 0 ? `${msObject['h']}:` : ''}${
+            msObject['h'] > 0
+                ? msObject['m'].toString().padStart(2, '0')
+                : msObject['m']
+        }:${msObject['s'].toString().padStart(2, '0')}`;
+    }
+    return `${
+        msObject['d'] > 0
+            ? `${msObject['d']} day${msObject['d'] === 1 ? '' : 's'}, `
+            : ''
+    }${
+        msObject['h'] > 0
+            ? `${msObject['h']} hr${msObject['h'] === 1 ? '' : 's'}, `
+            : ''
+    }${
+        msObject['m'] > 0
+            ? `${msObject['m']} min${msObject['m'] === 1 ? '' : 's'}, `
+            : ''
+    }${
+        msObject['s'] > 0
+            ? `${msObject['s']} sec${msObject['s'] === 1 ? '' : 's'}, `
+            : ''
+    }`.slice(0, -2);
 }
 
 /**
@@ -79,28 +79,28 @@ export function msToTimeString(
  * @returns The parsed milliseconds.
  */
 export function parseTimeString(timeString: string): number {
-	const timeRegex = /(\d+)([smhd])/g;
-	const timeMatches = timeString.matchAll(timeRegex);
-	let time = 0;
-	for (const match of timeMatches) {
-		const amount = parseInt(match[1]);
-		const unit = match[2];
-		switch (unit) {
-			case 's':
-				time += amount * 1000;
-				break;
-			case 'm':
-				time += amount * 1000 * 60;
-				break;
-			case 'h':
-				time += amount * 1000 * 60 * 60;
-				break;
-			case 'd':
-				time += amount * 1000 * 60 * 60 * 24;
-				break;
-		}
-	}
-	return time;
+    const timeRegex = /(\d+)([smhd])/g;
+    const timeMatches = timeString.matchAll(timeRegex);
+    let time = 0;
+    for (const match of timeMatches) {
+        const amount = parseInt(match[1]);
+        const unit = match[2];
+        switch (unit) {
+            case 's':
+                time += amount * 1000;
+                break;
+            case 'm':
+                time += amount * 1000 * 60;
+                break;
+            case 'h':
+                time += amount * 1000 * 60 * 60;
+                break;
+            case 'd':
+                time += amount * 1000 * 60 * 60 * 24;
+                break;
+        }
+    }
+    return time;
 }
 
 /**
@@ -111,18 +111,18 @@ export function parseTimeString(timeString: string): number {
  * @returns The rounded number.
  */
 export function roundTo(n: number, digits?: number): number {
-	let negative = false;
-	if (digits === undefined) digits = 0;
-	if (n < 0) {
-		negative = true;
-		n = n * -1;
-	}
-	const multiplicator = Math.pow(10, digits);
-	n = parseFloat((n * multiplicator).toFixed(11));
-	n = parseFloat((Math.round(n) / multiplicator).toFixed(digits));
-	if (negative) n = parseFloat((n * -1).toFixed(digits));
-	if (digits === 0) n = parseInt(n.toString(), 10);
-	return n;
+    let negative = false;
+    if (digits === undefined) digits = 0;
+    if (n < 0) {
+        negative = true;
+        n = n * -1;
+    }
+    const multiplicator = Math.pow(10, digits);
+    n = parseFloat((n * multiplicator).toFixed(11));
+    n = parseFloat((Math.round(n) / multiplicator).toFixed(digits));
+    if (negative) n = parseFloat((n * -1).toFixed(digits));
+    if (digits === 0) n = parseInt(n.toString(), 10);
+    return n;
 }
 
 /**
@@ -131,22 +131,22 @@ export function roundTo(n: number, digits?: number): number {
  * @returns The progress bar.
  */
 export function getBar(progress: number): string {
-	const progressBars = [
-		'🔘▬▬▬▬▬▬▬▬▬',
-		'▬🔘▬▬▬▬▬▬▬▬',
-		'▬▬🔘▬▬▬▬▬▬▬',
-		'▬▬▬🔘▬▬▬▬▬▬',
-		'▬▬▬▬🔘▬▬▬▬▬',
-		'▬▬▬▬▬🔘▬▬▬▬',
-		'▬▬▬▬▬▬🔘▬▬▬',
-		'▬▬▬▬▬▬▬🔘▬▬',
-		'▬▬▬▬▬▬▬▬🔘▬',
-		'▬▬▬▬▬▬▬▬▬🔘',
-	];
-	if (isNaN(progress) || progress < 10) return progressBars[0];
-	return progressBars[
-		Math.min(Math.floor(progress / 10), progressBars.length - 1)
-	];
+    const progressBars = [
+        '🔘▬▬▬▬▬▬▬▬▬',
+        '▬🔘▬▬▬▬▬▬▬▬',
+        '▬▬🔘▬▬▬▬▬▬▬',
+        '▬▬▬🔘▬▬▬▬▬▬',
+        '▬▬▬▬🔘▬▬▬▬▬',
+        '▬▬▬▬▬🔘▬▬▬▬',
+        '▬▬▬▬▬▬🔘▬▬▬',
+        '▬▬▬▬▬▬▬🔘▬▬',
+        '▬▬▬▬▬▬▬▬🔘▬',
+        '▬▬▬▬▬▬▬▬▬🔘',
+    ];
+    if (isNaN(progress) || progress < 10) return progressBars[0];
+    return progressBars[
+        Math.min(Math.floor(progress / 10), progressBars.length - 1)
+    ];
 }
 
 /**
@@ -157,20 +157,20 @@ export function getBar(progress: number): string {
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function paginate(arr: any[], size: number): any[][] {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	return arr.reduce((acc: any[], val: unknown, i: number): any[] => {
-		const idx = Math.floor(i / size);
-		const page = acc[idx] || (acc[idx] = []);
-		page.push(val);
-		return acc;
-	}, []);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return arr.reduce((acc: any[], val: unknown, i: number): any[] => {
+        const idx = Math.floor(i / size);
+        const page = acc[idx] || (acc[idx] = []);
+        page.push(val);
+        return acc;
+    }, []);
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function getJSONResponse(body: any): Promise<unknown> {
-	let fullBody = '';
-	for await (const d of body) {
-		fullBody += d.toString();
-	}
-	return JSON.parse(fullBody);
+    let fullBody = '';
+    for await (const d of body) {
+        fullBody += d.toString();
+    }
+    return JSON.parse(fullBody);
 }
