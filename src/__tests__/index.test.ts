15a6468fbc6b528fddc053516b5199ed2f172e70
Max <max@zptx.dev>
2022-12-24 21:13:57 +0800

    test: update test file

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -8,30 +8,30 @@ import {
     roundTo,
 } from '../index.js';
 
-describe('msToTime function', () => {
-    test('converts milliseconds to a time object', () => {
+describe('msToTime function', (): void => {
+    test('converts milliseconds to a time object', (): void => {
         expect(msToTime(3600000)).toEqual({ d: 0, h: 1, m: 0, s: 0 });
     });
 
-    test('converts milliseconds to seconds', () => {
+    test('converts milliseconds to seconds', (): void => {
         expect(msToTime(3600000, 's')).toBe(3600);
     });
 
-    test('converts milliseconds to minutes', () => {
+    test('converts milliseconds to minutes', (): void => {
         expect(msToTime(3600000, 'm')).toBe(60);
     });
 
-    test('converts milliseconds to hours', () => {
+    test('converts milliseconds to hours', (): void => {
         expect(msToTime(3600000, 'h')).toBe(1);
     });
 
-    test('converts milliseconds to days', () => {
+    test('converts milliseconds to days', (): void => {
         expect(msToTime(86400000, 'd')).toBe(1);
     });
 });
 
-describe('msToTimeString function', () => {
-    test('returns a simple string for less than a day', () => {
+describe('msToTimeString function', (): void => {
+    test('returns a simple string for less than a day', (): void => {
         expect(msToTimeString({ d: 0, h: 0, m: 0, s: 5 }, true)).toBe('0:05');
         expect(msToTimeString({ d: 0, h: 0, m: 5, s: 5 }, true)).toBe('5:05');
         expect(msToTimeString({ d: 0, h: 5, m: 5, s: 5 }, true)).toBe(
@@ -39,7 +39,7 @@ describe('msToTimeString function', () => {
         );
     });
 
-    test('returns "MORE_THAN_A_DAY" for more than a day', () => {
+    test('returns "MORE_THAN_A_DAY" for more than a day', (): void => {
         expect(msToTimeString({ d: 1, h: 0, m: 0, s: 0 }, true)).toBe(
             'MORE_THAN_A_DAY',
         );
@@ -51,7 +51,7 @@ describe('msToTimeString function', () => {
         );
     });
 
-    test('returns a detailed string for less than a day', () => {
+    test('returns a detailed string for less than a day', (): void => {
         expect(msToTimeString({ d: 0, h: 0, m: 0, s: 5 })).toBe('5 secs');
         expect(msToTimeString({ d: 0, h: 0, m: 5, s: 5 })).toBe(
             '5 mins, 5 secs',
@@ -64,7 +64,7 @@ describe('msToTimeString function', () => {
         );
     });
 
-    test('returns a detailed string for more than a day', () => {
+    test('returns a detailed string for more than a day', (): void => {
         expect(msToTimeString({ d: 1, h: 0, m: 0, s: 0 })).toBe('1 day');
         expect(msToTimeString({ d: 2, h: 0, m: 0, s: 0 })).toBe('2 days');
         expect(msToTimeString({ d: 1, h: 5, m: 5, s: 5 })).toBe(
@@ -73,104 +73,104 @@ describe('msToTimeString function', () => {
     });
 });
 
-describe('parseTimeString function', () => {
-    test('parses simple time strings correctly', () => {
+describe('parseTimeString function', (): void => {
+    test('parses simple time strings correctly', (): void => {
         expect(parseTimeString('1s')).toBe(1000);
         expect(parseTimeString('1m')).toBe(60000);
         expect(parseTimeString('1h')).toBe(3600000);
         expect(parseTimeString('1d')).toBe(86400000);
     });
 
-    test('parses multiple time units correctly', () => {
+    test('parses multiple time units correctly', (): void => {
         expect(parseTimeString('1h1m1s')).toBe(3661000);
         expect(parseTimeString('2d3h4m5s')).toBe(183845000);
     });
 
-    test('parses time strings with multiple of the same unit correctly', () => {
+    test('parses time strings with multiple of the same unit correctly', (): void => {
         expect(parseTimeString('2s2s')).toBe(4000);
         expect(parseTimeString('3m3m3m')).toBe(540000);
     });
 
-    test('parses time strings with no units correctly', () => {
+    test('parses time strings with no units correctly', (): void => {
         expect(parseTimeString('0')).toBe(0);
     });
 });
 
-describe('roundTo function', () => {
-    it('should round numbers to the specified number of decimal places', () => {
+describe('roundTo function', (): void => {
+    it('should round numbers to the specified number of decimal places', (): void => {
         expect(roundTo(3.1415, 2)).toBe(3.14);
         expect(roundTo(3.1415, 3)).toBe(3.142);
         expect(roundTo(5.678, 0)).toBe(6);
     });
 
-    it('should round negative numbers correctly', () => {
+    it('should round negative numbers correctly', (): void => {
         expect(roundTo(-3.1415, 2)).toBe(-3.14);
         expect(roundTo(-3.1415, 3)).toBe(-3.142);
         expect(roundTo(-5.678, 0)).toBe(-6);
     });
 
-    it('should round to 0 decimal places by default', () => {
+    it('should round to 0 decimal places by default', (): void => {
         expect(roundTo(3.1415)).toBe(3);
         expect(roundTo(-3.1415)).toBe(-3);
     });
 });
 
-describe('getBar function', () => {
-    it('should return the correct progress bar for progress percentage less than 10', () => {
+describe('getBar function', (): void => {
+    it('should return the correct progress bar for progress percentage less than 10', (): void => {
         expect(getBar(9)).toBe('🔘▬▬▬▬▬▬▬▬▬');
         expect(getBar(5)).toBe('🔘▬▬▬▬▬▬▬▬▬');
         expect(getBar(0)).toBe('🔘▬▬▬▬▬▬▬▬▬');
     });
 
-    it('should return the correct progress bar for progress percentage between 10 and 19', () => {
+    it('should return the correct progress bar for progress percentage between 10 and 19', (): void => {
         expect(getBar(10)).toBe('▬🔘▬▬▬▬▬▬▬▬');
         expect(getBar(15)).toBe('▬🔘▬▬▬▬▬▬▬▬');
         expect(getBar(19)).toBe('▬🔘▬▬▬▬▬▬▬▬');
     });
 
-    it('should return the correct progress bar for progress percentage between 20 and 29', () => {
+    it('should return the correct progress bar for progress percentage between 20 and 29', (): void => {
         expect(getBar(20)).toBe('▬▬🔘▬▬▬▬▬▬▬');
         expect(getBar(25)).toBe('▬▬🔘▬▬▬▬▬▬▬');
         expect(getBar(29)).toBe('▬▬🔘▬▬▬▬▬▬▬');
     });
 
-    it('should return the correct progress bar for progress percentage between 30 and 39', () => {
+    it('should return the correct progress bar for progress percentage between 30 and 39', (): void => {
         expect(getBar(30)).toBe('▬▬▬🔘▬▬▬▬▬▬');
         expect(getBar(35)).toBe('▬▬▬🔘▬▬▬▬▬▬');
         expect(getBar(39)).toBe('▬▬▬🔘▬▬▬▬▬▬');
     });
 
-    it('should return the correct progress bar for progress percentage between 40 and 49', () => {
+    it('should return the correct progress bar for progress percentage between 40 and 49', (): void => {
         expect(getBar(40)).toBe('▬▬▬▬🔘▬▬▬▬▬');
         expect(getBar(45)).toBe('▬▬▬▬🔘▬▬▬▬▬');
         expect(getBar(49)).toBe('▬▬▬▬🔘▬▬▬▬▬');
     });
 
-    it('should return the correct progress bar for progress percentage between 50 and 59', () => {
+    it('should return the correct progress bar for progress percentage between 50 and 59', (): void => {
         expect(getBar(50)).toBe('▬▬▬▬▬🔘▬▬▬▬');
         expect(getBar(55)).toBe('▬▬▬▬▬🔘▬▬▬▬');
         expect(getBar(59)).toBe('▬▬▬▬▬🔘▬▬▬▬');
     });
 
-    it('should return the correct progress bar for progress percentage between 60 and 69', () => {
+    it('should return the correct progress bar for progress percentage between 60 and 69', (): void => {
         expect(getBar(60)).toBe('▬▬▬▬▬▬🔘▬▬▬');
         expect(getBar(65)).toBe('▬▬▬▬▬▬🔘▬▬▬');
         expect(getBar(69)).toBe('▬▬▬▬▬▬🔘▬▬▬');
     });
 
-    it('should return the correct progress bar for progress percentage between 70 and 79', () => {
+    it('should return the correct progress bar for progress percentage between 70 and 79', (): void => {
         expect(getBar(70)).toBe('▬▬▬▬▬▬▬🔘▬▬');
         expect(getBar(75)).toBe('▬▬▬▬▬▬▬🔘▬▬');
         expect(getBar(79)).toBe('▬▬▬▬▬▬▬🔘▬▬');
     });
 
-    it('should return the correct progress bar for progress percentage between 80 and 89', () => {
+    it('should return the correct progress bar for progress percentage between 80 and 89', (): void => {
         expect(getBar(80)).toBe('▬▬▬▬▬▬▬▬🔘▬');
         expect(getBar(85)).toBe('▬▬▬▬▬▬▬▬🔘▬');
         expect(getBar(89)).toBe('▬▬▬▬▬▬▬▬🔘▬');
     });
 
-    it('should return the correct progress bar for progress percentage between 90 and 100', () => {
+    it('should return the correct progress bar for progress percentage between 90 and 100', (): void => {
         expect(getBar(90)).toBe('▬▬▬▬▬▬▬▬▬🔘');
         expect(getBar(95)).toBe('▬▬▬▬▬▬▬▬▬🔘');
         expect(getBar(99)).toBe('▬▬▬▬▬▬▬▬▬🔘');
@@ -178,8 +178,8 @@ describe('getBar function', () => {
     });
 });
 
-describe('paginate function', () => {
-    test('paginates an array of size 10 into pages of size 5', () => {
+describe('paginate function', (): void => {
+    test('paginates an array of size 10 into pages of size 5', (): void => {
         const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         const paginated = paginate(arr, 5);
         expect(paginated).toEqual([
@@ -188,7 +188,7 @@ describe('paginate function', () => {
         ]);
     });
 
-    test('paginates an array of size 9 into pages of size 5', () => {
+    test('paginates an array of size 9 into pages of size 5', (): void => {
         const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         const paginated = paginate(arr, 5);
         expect(paginated).toEqual([
@@ -197,7 +197,7 @@ describe('paginate function', () => {
         ]);
     });
 
-    test('paginates an array of size 5 into pages of size 3', () => {
+    test('paginates an array of size 5 into pages of size 3', (): void => {
         const arr = [1, 2, 3, 4, 5];
         const paginated = paginate(arr, 3);
         expect(paginated).toEqual([
@@ -206,32 +206,32 @@ describe('paginate function', () => {
         ]);
     });
 
-    test('paginates an array of size 4 into pages of size 3', () => {
+    test('paginates an array of size 4 into pages of size 3', (): void => {
         const arr = [1, 2, 3, 4];
         const paginated = paginate(arr, 3);
         expect(paginated).toEqual([[1, 2, 3], [4]]);
     });
 
-    test('paginates an empty array', () => {
-        const arr: any[] = [];
+    test('paginates an empty array', (): void => {
+        const arr: string[] = [];
         const paginated = paginate(arr, 3);
         expect(paginated).toEqual([]);
     });
 });
 
-describe('getJSONResponse function', () => {
-    test('getJSONResponse should return a parsed JSON object', async () => {
+describe('getJSONResponse function', (): void => {
+    test('getJSONResponse should return a parsed JSON object', async (): Promise<void> => {
         const body = '{"a": 1, "b": 2}';
         const result = await getJSONResponse(body);
         expect(result).toEqual({ a: 1, b: 2 });
     });
 
-    test('getJSONResponse should throw an error for an empty body', async () => {
+    test('getJSONResponse should throw an error for an empty body', async (): Promise<void> => {
         const body = '';
         await expect(getJSONResponse(body)).rejects.toThrow();
     });
 
-    test('getJSONResponse should throw an error for invalid JSON', async () => {
+    test('getJSONResponse should throw an error for invalid JSON', async (): Promise<void> => {
         const body = '{"a": 1, b: 2}';
         await expect(getJSONResponse(body)).rejects.toThrow();
     });
